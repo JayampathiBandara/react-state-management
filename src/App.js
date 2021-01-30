4b0ce0637b13d5bc11c2b8d650a7f4397bc6df5d
jayampathib <wmjb.weerasinghe@gmail.com>
2021-01-30 23:41:51 +0800

    1.6 use json object and display data in borser[no api call]

--- src/App.js
+++ src/App.js
@@ -2,8 +2,21 @@ import React from "react";
 import "./App.css";
 import Footer from "./Footer";
 import Header from "./Header";
+import productsData from "./services/products.data";
 
 export default function App() {
+  function renderProduct(p) {
+    return (
+      <div key={p.id} className="product">
+        <a href="/">
+          <img src={`/images/${p.image}`} alt={p.name} />
+          <h3>{p.name}</h3>
+          <p>${p.price}</p>
+        </a>
+      </div>
+    );
+  }
+
   return (
     <>
       <div className="content">
@@ -18,6 +31,7 @@ export default function App() {
               <option value="9">9</option>
             </select>
           </section>
+          <section id="products">{productsData.map(renderProduct)}</section>
         </main>
       </div>
       <Footer />
